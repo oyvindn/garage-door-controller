8225330b3697f64c5cea6ba6f3f508a3d500b976
Øyvind Nerbråten <oyvind.nerbraten@bekk.no>
2019-03-27 20:28:26 +0100

    Changing to 4 space indentation

--- garage-door-controller.ino
+++ garage-door-controller.ino
@@ -8,75 +8,75 @@ WiFiClientSecure wifiClientSecure;
 PubSubClient mqttPubSubClient(mqtt_broker_host, mqtt_broker_port, &subscriptionCallback, wifiClientSecure);
 
 void setup() {
-  Serial.begin(115200);
-  delay(10);
-  connectToWifi();
-  
-  wifiClientSecure.setCACert(root_ca);
+    Serial.begin(115200);
+    delay(10);
+    connectToWifi();
 
-  pinMode(door_open_magnetic_sensor_gpio, INPUT);
-  pinMode(door_closed_magnetic_sensor_gpio, INPUT);
-  pinMode(door_motor_relay_switch_gpio, OUTPUT);
+    wifiClientSecure.setCACert(root_ca);
+
+    pinMode(door_open_magnetic_sensor_gpio, INPUT);
+    pinMode(door_closed_magnetic_sensor_gpio, INPUT);
+    pinMode(door_motor_relay_switch_gpio, OUTPUT);
 }
 
 void loop() {
-  ensureMqttConnection(mqttPubSubClient);
-  mqttPubSubClient.loop();
+    ensureMqttConnection(mqttPubSubClient);
+    mqttPubSubClient.loop();
 }
 
 void connectToWifi() {
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
 
-  Serial.println();
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+    Serial.println();
+    Serial.println("WiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void ensureMqttConnection(PubSubClient &mqttClient) {
-  if (!mqttClient.connected()) {
-    connectToMqttBroker(mqttClient);
-  }
+    if (!mqttClient.connected()) {
+        connectToMqttBroker(mqttClient);
+    }
 }
 
 void connectToMqttBroker(PubSubClient &mqttClient) {
-  while (!mqttClient.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    
-    if (mqttClient.connect("garage-door-controller")) {
-      Serial.println("connected");
-      subscribeOnConnect(mqttClient);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqttClient.state());
-      Serial.println(" trying again in 5 seconds");
-      delay(5000);
+    while (!mqttClient.connected()) {
+        Serial.print("Attempting MQTT connection...");
+
+        if (mqttClient.connect("garage-door-controller")) {
+            Serial.println("connected");
+            subscribeOnConnect(mqttClient);
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(mqttClient.state());
+            Serial.println(" trying again in 5 seconds");
+            delay(5000);
+        }
     }
-  }
 }
 
 void subscribeOnConnect(PubSubClient &mqttClient) {
-  mqttClient.subscribe(mqtt_garage_door_trigger_topic);
+    mqttClient.subscribe(mqtt_garage_door_trigger_topic);
 }
 
 void subscriptionCallback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
+    Serial.print("Message arrived on topic: ");
+    Serial.print(topic);
+    Serial.print(". Message: ");
+    String messageTemp;
+
+    for (int i = 0; i < length; i++) {
+        Serial.print((char)message[i]);
+        messageTemp += (char)message[i];
+    }
+    Serial.println();
 }
