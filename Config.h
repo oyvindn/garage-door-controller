e7148323890660bb2668aa0a72f8467717a8993c
Øyvind Nerbråten <oyvind.nerbraten@bekk.no>
2020-10-29 11:03:20 +0100

    GPIO setup for SMD Rev. A

--- Config.h
+++ Config.h
@@ -6,18 +6,25 @@ const char* ssid     = "WIFI_SSID";
 const char* password = "WIFI_PASSWORD";
 
 // IO pins settings
-const int garage_door_open_magnetic_sensor_gpio   = 33;
-const int garage_door_closed_magnetic_sensor_gpio = 15;
-const int garage_door_opener_active_sensor_gpio   = 32;
-const int garage_door_opener_relay_switch_gpio    = 21;
+const int garage_door_opener_relay_switch_gpio = 13;
+
+const int garage_door_open_magnetic_sensor_gpio   = 17;
+const int garage_door_closed_magnetic_sensor_gpio = 18;
+const int garage_door_opener_active_sensor_gpio   = 19;
+
+const int garage_door_open_indicator__green_led_gpio          = 21;
+const int garage_door_opener_active_indicator_yellow_led_gpio = 22;
+const int garage_door_closed_indicator_red_led_gpio           = 23;
+
+const int garage_outside_temp_sensor_gpio = 27;
 
 // MQTT broker settings
 const char* mqtt_broker_host = "MQTT_HOST";
 const int mqtt_broker_port   = 8883;
 
 // MQTT topics
-const char* garage_door_opener_control_topic       = "home/garage/door-opener/control";
-const char* garage_door_current_state_topic        = "home/garage/door/current-state";
+const char* garage_door_opener_control_topic = "home/garage/door-opener/control";
+const char* garage_door_current_state_topic  = "home/garage/door/current-state";
 
 // TLS settings
 const char* root_ca_certificate = \
